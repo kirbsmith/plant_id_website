5b3a50070218a3a3fac9a404c9f08949515930f6
Kirby <kirbsmith@gmail.com>
2022-05-27 07:50:38 -0400

    update.2

--- js/main.js
+++ js/main.js
@@ -52,11 +52,11 @@ function sendIdentification() {
         var commonNameElement = document.getElementById('plant_information')
         commonNameElement.appendChild(commonName)
 
-        var scientificName = document.createElement("p")
-        var text = document.createTextNode(data.suggestions[0].plant_details.scientific_name)
-        scientificName.appendChild(text)
-        var element = document.getElementById('plant_information')
-        element.appendChild(scientificName)
+        // var scientificName = document.createElement("p")
+        // var text = document.createTextNode(data.suggestions[0].plant_details.scientific_name)
+        // scientificName.appendChild(text)
+        // var element = document.getElementById('plant_information')
+        // element.appendChild(scientificName)
 
         var img = document.createElement("img")
         img.src = data.suggestions[0].similar_images[0].url
